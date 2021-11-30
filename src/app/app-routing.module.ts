8eee64a3851b3da3446468061424fdb7127e346b
william <63308895+williamBAlex@users.noreply.github.com>
2021-11-29 23:06:00 -0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InformeReparacionComponent } from './taller/informe-reparacion/informe-reparacion.component';
+import { RepuestoAgregadoComponent } from './taller/repuesto-agregado/repuesto-agregado.component';
+import { SolicitudRepuestoComponent } from './taller/solicitud-repuesto/solicitud-repuesto.component';
+import { TallerComponent } from './taller/taller.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/taller',
+    component: TallerComponent
+  },
+  {
+    path: "/taller/informeReparacion",
+    component: InformeReparacionComponent
+  },
+  {
+    path: "/taller/repuestoAgregado",
+    component: RepuestoAgregadoComponent
+  },
+  {
+    path: "/taller/solicitudRepuesto",
+    component: SolicitudRepuestoComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/taller/taller.component.ts
+++ src/app/taller/taller.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-taller',
-  templateUrl: './taller.component.html',
+  templateUrl: 'taller.component.html',
   styleUrls: ['./taller.component.css']
 })
 export class TallerComponent implements OnInit {
