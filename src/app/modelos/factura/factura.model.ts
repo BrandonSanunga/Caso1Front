b1825ddea84869635c0b90b575f0c24ac1f26942
skull <jhosepandres14@gmail.com>
2021-12-21 12:52:16 -0500

    lista la venta de vehiculos
    REALIZAR  UN "NPM INSTALL " INSTALE UNA LIBRERIA DE SWEET ALERT

--- src/app/modelos/factura/factura.model.ts
+++ src/app/modelos/factura/factura.model.ts
@@ -5,9 +5,18 @@ export class Factura {
   id?: string;
   fecha_emision?: string;
   descripcion?: string;
-  ruc_factura?: string;
-  nombre_empresa?: string;
-  direccion?: string;
-  client?: Clientes;
+  ruc_factura?: string = "1234567890";
+  nombre_empresa?: string = "STAR MOTORS";
+  direccion?: string = "Av Americas";
+  cliente?: Clientes;
   detallesfacturas?: Array<DetalleFactura> = [];
+  total?: number;
+
+  public calcularGranTotal(): number {
+    this.total = 0;
+    this.detallesfacturas?.forEach((det: DetalleFactura) => {
+      this.total! += det.calcularImporte();
+    });
+    return this.total;
+  }
 }
