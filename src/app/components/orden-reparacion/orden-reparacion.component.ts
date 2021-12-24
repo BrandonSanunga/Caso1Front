711beab040eb5132e408e74ae4aef66c5a5c11c4
andresGuaman <andres.guaman.est@tecazuay.edu.ec>
2021-12-23 23:49:28 -0500

    orden reparacion

--- src/app/components/orden-reparacion/orden-reparacion.component.ts
+++ src/app/components/orden-reparacion/orden-reparacion.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { OrdenRepCavecera } from 'src/app/modelos/ordenReclamo/orden-rep-cavecera';
+import { OrdenRepCuerpo } from 'src/app/modelos/ordenReclamo/orden-rep-cuerpo';
+import { OrdenRepCaveServiceService } from 'src/app/services/ordenReparacion/orden-rep-cave-service.service';
 
 @Component({
   selector: 'app-orden-reparacion',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orden-reparacion.component.css']
 })
 export class OrdenReparacionComponent implements OnInit {
+  idordenCuerpo:any;
+  fecha_emision:any;
+  fecha_ingreso:any;
+  trabajoSolicitado:any;
+  trabajoRealizar:any;
+  observaciones:any;
+  velocidades: any;
+  inspeCuerpo: any;
+  ordenform!: FormGroup;
+ordenList: any;
+  cliente: any;
+ ordenCuerpos:OrdenRepCuerpo =new OrdenRepCuerpo ();
+  ordenCave:OrdenRepCavecera =new OrdenRepCavecera();
 
-  constructor() { }
+  constructor(public fb: FormBuilder,public ordenReparacion: OrdenRepCaveServiceService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  guardarOrden(): void{
+
   }
-
+ editarOrden(){
+    console.log(this.ordenReparacion)
+    this.ordenReparacion.update(this.ordenReparacion.buscarId,this.ordenReparacion).subscribe(data=>{
+      console.log(data)
+    })
+  }
 }
