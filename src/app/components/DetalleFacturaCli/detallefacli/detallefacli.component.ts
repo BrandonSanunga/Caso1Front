8b94fa535a3e1003ad82dbb2ffc6181a7472dd29
skull <jhosepandres14@gmail.com>
2021-12-26 15:42:32 -0500

    pdf en list de facturas
    hacer un npm i se instalo una libreria

--- src/app/components/DetalleFacturaCli/detallefacli/detallefacli.component.ts
+++ src/app/components/DetalleFacturaCli/detallefacli/detallefacli.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Columns, PdfMakeWrapper, Txt } from "pdfmake-wrapper";
 import { Factura } from "src/app/modelos/factura/factura.model";
 import { FacturaService } from "src/app/services/Factura/factura.service";
+import { UtilReportService } from "src/app/services/Factura/util-report.service";
 import Swal from "sweetalert2";
 
 @Component({
@@ -10,8 +12,12 @@ import Swal from "sweetalert2";
 })
 export class DetallefacliComponent implements OnInit {
   public facturas: Factura[] = [];
+  public factura = new Factura();
 
-  constructor(private facturaService: FacturaService) {}
+  constructor(
+    private facturaService: FacturaService,
+    private srvUr: UtilReportService
+  ) {}
 
   ngOnInit(): void {
     this.cargarFacturas();
@@ -54,5 +60,84 @@ export class DetallefacliComponent implements OnInit {
           });
         }
       });
+  }
+
+  public reporteFacturaPDF(factura: Factura) {
+    this.facturaService.getFacturaById(factura.id!).subscribe((factura) => {
+      this.factura = factura;
+      const pdf = new PdfMakeWrapper();
+      pdf.pageMargins([40, 60, 40, 60]);
+      pdf.pageSize("A4");
+      pdf.info({
+        title: "Factura",
+        author: "START-MOTORS",
+        subject: "Factura de Vehiculos",
+      });
+      pdf.add(
+        new Txt(`Empresa:${this.factura.nombre_empresa}`)
+          .alignment("left")
+          .italics()
+          .bold().end
+      );
+      pdf.add(
+        new Txt(`${this.factura.fecha_emision}`)
+          .alignment("left")
+          .italics()
+          .bold().end
+      );
+
+      pdf.add(
+        new Txt(`Ruc:${this.factura.ruc_factura}`)
+          .alignment("left")
+          .italics()
+          .bold().end
+      );
+      pdf.add(
+        new Txt(`Direccion:${this.factura.direccion}`)
+          .alignment("left")
+          .italics()
+          .bold().end
+      );
+      pdf.add(
+        new Txt(`Cliente:${this.factura.cliente?.nombresClient}`)
+          .alignment("left")
+          .bold()
+          .italics().end
+      );
+      pdf.add(pdf.ln(1));
+      pdf.add(new Txt("Factura").alignment("left").bold().italics().end);
+      pdf.add(pdf.ln(1));
+      pdf.add(
+        new Columns(["Vehiculo", "Cantidad", "Precio U", "SubTotal"])
+          .columnGap(3)
+          .style("text-center")
+          .bold().end
+      );
+      this.factura.detallesfacturas?.forEach((detFac) => {
+        pdf.add(
+          new Columns([
+            ` ${detFac.vehiculo?.vehiculoCatalogo?.diseno?.marca} ${detFac.vehiculo?.vehiculoCatalogo?.diseno?.modelo} ${detFac.vehiculo?.vehiculoCatalogo?.year_vehiculo} `,
+            detFac.cantidad,
+            `$${this.srvUr.formateaValor(detFac.vehiculo?.precio_venta)}`,
+            `$${this.srvUr.formateaValor(
+              detFac.vehiculo?.precio_venta! * detFac.cantidad!
+            )}`,
+          ])
+            .columnGap(3)
+            .style("text-center").end
+        );
+      });
+      let total = 0;
+      this.factura.detallesfacturas?.forEach((detFac) => {
+        total += detFac.vehiculo?.precio_venta! * detFac.cantidad!;
+      });
+      pdf.add(
+        new Columns(["Total", "", "", `$${this.srvUr.formateaValor(total)}`])
+          .columnGap(3)
+          .style("text-center")
+          .bold().end
+      );
+      pdf.create().open();
+    });
   }
 }
