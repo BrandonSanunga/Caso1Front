33f953103ce00d5006eb6785adb0de98af2d6c83
Andy2498 <andyjara18@gmail.com>
2021-12-27 23:25:02 -0500

    Cmb_Inf

--- src/app/services/ordenReparacion/orden-rep-cuerp-service.service.ts
+++ src/app/services/ordenReparacion/orden-rep-cuerp-service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdenRepCuerpServiceService {
+  private API_SEVER = "http://localhost:8080/ordecuerpo/api/v1/getall";
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllordenes():Observable<any>{
+    return this.httpClient.get(this.API_SEVER);
+  }
 }
